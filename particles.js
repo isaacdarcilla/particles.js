d79119ebff3538068f0fcab7aae235ebcf5d764d
Gennadiy Popov <popov.gennadiy.87@gmail.com>
2023-09-17 21:11:36 +0300

    Update particles.js

--- particles.js
+++ particles.js
@@ -1490,7 +1490,7 @@ window.particlesJS = function(tag_id, params){
   }
 
   /* pJS elements */
-  var pJS_tag = document.getElementById(tag_id),
+  var pJS_tag = typeof tag_id === 'string' ? document.getElementById(tag_id) : tag_id,
       pJS_canvas_class = 'particles-js-canvas-el',
       exist_canvas = pJS_tag.getElementsByClassName(pJS_canvas_class);
 
